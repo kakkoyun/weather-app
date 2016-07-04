64cb0d90b5317fd490aa0a617600ecb5988d507c
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-04 13:24:01 +0300

    Add redux-promise middleware. Modify project to work with redux dev tools for Chrome.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(
+  (window.devToolsExtension
+    ? window.devToolsExtension()(createStore)
+    : createStore));
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
